ed91e6cb285e23b34e0a72b02db147c9d527b734
María José Benites Gaitán <mariajosebg95@gmail.com>
2020-03-02 12:54:24 +0100

    Creación de Utilidad.cs

---
+++ WebApplicationApiChrysallis/Utilidades/Utilidad.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace WebApplicationApiChrysallis.Utilidades
+{
+    public static class Utilidad
+    {
+        public static String MensajeError(SqlException sqlEx)
+        {
+            String mensaje;
+            switch (sqlEx.Number)
+            {
+                case -1:
+                    mensaje = "Error de conexión con el servidor";
+                    break;
+                case 547:
+                    mensaje = "Tiene datos relacionados";
+                    break;
+                case 2601:
+                    mensaje = "Datos duplicados";
+                    break;
+                case 2627:
+                    mensaje = "Datos duplicados";
+                    break;
+                case 4060:
+                    mensaje = "No se encuentra la base de datos";
+                    break;
+                case 18456:
+                    mensaje = "Usuario o contraseña incorrectos";
+                    break;
+                default:
+                    mensaje = sqlEx.Number + " - " + sqlEx.Message;
+                    break;
+            }
+            return mensaje;
+        }
+    }
+}
