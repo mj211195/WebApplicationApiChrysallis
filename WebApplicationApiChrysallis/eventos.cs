ec2b74f4ab5a88f06eb835382ba40cf1fb4885da
María José Benites Gaitán <mariajosebg95@gmail.com>
2020-03-05 13:50:50 +0100

    Modelo actualización

--- WebApplicationApiChrysallis/eventos.cs
+++ WebApplicationApiChrysallis/eventos.cs
@@ -29,6 +29,9 @@ namespace WebApplicationApiChrysallis
         public Nullable<System.DateTime> fechaLimite { get; set; }
         public int numAsistentes { get; set; }
         public int id_comunidad { get; set; }
+        public string nombre { get; set; }
+        public byte[] imagen { get; set; }
+        public string descripcion { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<asistir> asistir { get; set; }
