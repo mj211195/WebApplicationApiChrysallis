ba72e4883265c9c26fc8a927e0513b4a1013301f
María José Benites Gaitán <mariajosebg95@gmail.com>
2020-04-13 23:56:18 +0200

    Cambios en eventosController

--- WebApplicationApiChrysallis/Controllers/EventosController.cs
+++ WebApplicationApiChrysallis/Controllers/EventosController.cs
@@ -156,7 +156,7 @@ namespace WebApplicationApiChrysallis.Controllers
             IHttpActionResult result;
             db.Configuration.LazyLoadingEnabled = false;
             List<eventos>_eventos = (
-                from e in db.eventos.Include("comunidades").Include("asistir").Include("notificaciones")
+                from e in db.eventos.Include("comunidades").Include("asistir").Include("notificaciones").Include("mensajes")
                 where e.id_comunidad == id_comunidad && e.nombre.Contains(nombre) && e.fecha >= date
                 select e).ToList();
 
@@ -183,7 +183,7 @@ namespace WebApplicationApiChrysallis.Controllers
             IHttpActionResult result;
             db.Configuration.LazyLoadingEnabled = false;
             List<eventos> _eventos = (
-                from e in db.eventos.Include("comunidades").Include("asistir").Include("notificaciones")
+                from e in db.eventos.Include("comunidades").Include("asistir").Include("notificaciones").Include("mensajes")
                 where e.id_comunidad == id_comunidad && e.fecha.Equals(date)
                 select e).ToList();
 
@@ -206,7 +206,7 @@ namespace WebApplicationApiChrysallis.Controllers
             IHttpActionResult result;
             db.Configuration.LazyLoadingEnabled = false;
             List<eventos> _eventos = (
-                from e in db.eventos.Include("comunidades").Include("asistir").Include("notificaciones")
+                from e in db.eventos.Include("comunidades").Include("asistir").Include("notificaciones").Include("mensajes")
                 where e.nombre.Contains(nombre) && e.id_comunidad == id_comunidad && e.fecha.Equals(date)
                 select e).ToList();
 
@@ -229,7 +229,7 @@ namespace WebApplicationApiChrysallis.Controllers
             IHttpActionResult result;
             db.Configuration.LazyLoadingEnabled = false;
             List<eventos> _eventos = (
-                from e in db.eventos.Include("comunidades").Include("asistir").Include("notificaciones")
+                from e in db.eventos.Include("comunidades").Include("asistir").Include("notificaciones").Include("mensajes")
                 where e.id_comunidad == id_comunidad && e.fecha >= date
                 select e).ToList();
 
